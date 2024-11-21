d0298ef0ddab65eb936d05bd8abb2ce9badaa8b0
manylie <sorrinka@gmail.com>
2024-11-20 20:35:30 -0500

    manuPro

---
+++ build/CMakeFiles/HolaMundo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HolaMundo.dir/main.cpp.obj"
+  "CMakeFiles/HolaMundo.dir/main.cpp.obj.d"
+  "HolaMundo.exe"
+  "HolaMundo.exe.manifest"
+  "HolaMundo.pdb"
+  "libHolaMundo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HolaMundo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/HolaMundo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HolaMundo.
+# This may be replaced when dependencies are built.
